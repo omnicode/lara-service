bebadb0a659d2b4810830b95f9e11d5622d87426
davitbek <davitzeynalyan1991@gmail.com>
2018-02-23 23:25:52 +0400

    Update CreatesApplication.php

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -14,7 +14,35 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = new \Illuminate\Foundation\Application(
+            realpath(__DIR__.'/../')
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Bind Important Interfaces
+        |--------------------------------------------------------------------------
+        |
+        | Next, we need to bind some important interfaces into the container so
+        | we will be able to resolve them when needed. The kernels serve the
+        | incoming requests to this application from both the web and CLI.
+        |
+        */
+
+        $app->singleton(
+            \Illuminate\Contracts\Http\Kernel::class,
+            \Illuminate\Foundation\Console\Kernel::class
+        );
+
+        $app->singleton(
+            \Illuminate\Contracts\Console\Kernel::class,
+            \Illuminate\Foundation\Console\Kernel::class
+        );
+
+        $app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            \Illuminate\Foundation\Exceptions\Handler::class
+        );
 
         $app->make(Kernel::class)->bootstrap();
 
